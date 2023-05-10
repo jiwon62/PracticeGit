909eded5755beb6d89f13ba07c910edd8cfe16e0
jiwoon <jwgil06@naver.com>
2023-05-10 10:24:23 +0900

    Initial Commit

---
+++ PracticeGit/PracticeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeGitApp.swift
+//  PracticeGit
+//
+//  Created by jeewony on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct PracticeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
