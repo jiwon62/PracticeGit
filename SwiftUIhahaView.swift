73606e71841493c8c0938ba4e78e78816024a835
jiwoon <jwgil06@naver.com>
2023-05-10 12:02:35 +0900

    new file

---
+++ SwiftUIhahaView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIhahaView.swift
+//  PracticeGit
+//
+//  Created by jeewony on 2023/05/10.
+//
+
+import SwiftUI
+
+struct SwiftUIhahaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIhahaView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIhahaView()
+    }
+}
